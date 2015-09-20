2a2a79c4e0a21f242f99099af6da09a924b3b533
19r3ka <yemahiatsi@gmail.com>
2015-09-20 10:45:42 +0200

    Adds a non-tested user profile.

--- zinfata/public/zinfataClient/app/components/users/usersService.js
+++ zinfata/public/zinfataClient/app/components/users/usersService.js
@@ -1,4 +1,4 @@
-app.service('UsersSvc', ['Users', 'Auth', 'MessageSvc', function(Users, Auth, MessageSvc) {
+app.service('UsersSvc', ['Users', 'Auth', 'MessageSvc', '$log', function(Users, Auth, MessageSvc, $log) {
   this.users    = Users.query();
   this.loggedIn = false;
 
@@ -13,17 +13,32 @@ app.service('UsersSvc', ['Users', 'Auth', 'MessageSvc', function(Users, Auth, Me
       // setUsers(users);
       MessageSvc.addMsg('success', 'Welcome to Zinfata, ' + saved_user.firstName);
     }, function(error) {
-      console.error('Unable to save user: ' + error);
+      $log.error('Unable to save user: ' + error);
       MessageSvc.addMsg('danger', 'Oops, we were unable to register you!');
     });
   };
 
+  this.update = function(user) {
+    var updatedUser = Users.get({id: user.id});
+    for(var key in user) {
+      if(updatedUser[key] !== user[key]) updatedUser = user[key];
+    }
+    updatedUser.$save(function(updatedUser) {
+      MessageSvc.addMsg('success', 'Your profile has been successfully updated');
+    },
+    function(error) {
+      $log.error('Profile update failed: ' + error);
+      MessageSvc.addMsg('danger', 'Profile update failed. Try again later!');
+    });
+  };
+
   this.login = function(user) {
-    Auth.login(user).then(function(data) {
-      alert('Logged user in!');
-      MessageSvc.addMsg('success', 'You are now logged in as ');
+    Auth.login(user).then(function(loggedUser) {
+      this.loggedIn = true;
+      $log.debug(angular.toJson(loggedUser, true));
+      MessageSvc.addMsg('success', 'You are now logged in as ' + loggedUser.data.firstName);
     }, function(error) {
-      console.error('Unable to log user in: ' + error);
+      $log.error('Unable to log user in: ' + error);
       MessageSvc.addMsg('danger', 'We were unable to log you in!');
     });
   };
@@ -34,14 +49,18 @@ app.service('UsersSvc', ['Users', 'Auth', 'MessageSvc', function(Users, Auth, Me
 
   this.currentUser = function() {
     Auth.currentUser.then(function(user) {
-      this.loggedIn = true;
       return user;
     }, function(error) {
       return {
+        id: 0,
         firstName: 'guest',
         lastName: 'user',
         handle: 'guestuser'
       };
     });
+  };
+
+  this.get = function(id) {
+    return Users.get({id: id});
   };
 }]);
