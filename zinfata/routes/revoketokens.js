460f8fdf1675b9f0e90021653607d545677a68d3
modeste <mo.assiongbon@gmail.com>
2016-01-21 20:25:45 +0000

    -Activate default "oauth2-server" error handler. -Update http status code return by oauth2 route to have a result similar to oauth2 spec. -Create route to get user metadata by providing an accesstoken. -Create proxy route to get user metadata by providing an accesstoken.

--- zinfata/routes/revoketokens.js
+++ zinfata/routes/revoketokens.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var auth = require('basic-auth');
+var oAuthClientModel = require('../models/OAuthClient');
 //var utils   = require('./../bin/utils');
 var oAuthAccessTokenModel   = require('./../models/OAuthAccessToken');
 var oAuthRefreshTokenModel   = require('./../models/OAuthRefreshToken');
@@ -36,10 +37,10 @@ router.post('/', function(req, res, next){
 		return next(error); 
 	}
 
-	if (!req.body.token_type_hint.match('refresh_token|acces_token')) {
+	if (!req.body.token_type_hint.match('refresh_token|access_token')) {
 		var error = new Error();
 		error.status = 400;
-		error.error_description = '\'token_type_hint\' parameter value must be either \'refresh_token\' or \'acces_token\'';
+		error.error_description = '\'token_type_hint\' parameter value must be either \'refresh_token\' or \'access_token\'';
 		error.error = 'invalid_request';
 		return next(error); 
 	}
@@ -52,24 +53,52 @@ router.post('/', function(req, res, next){
 		return next(error); 
 	}
 
-	var token_type_hint = req.body.token_type_hint;
-	var token = req.body.token;
+	//check client credentials
+	oAuthClientModel.getClient(client_id, client_secret, function(err, client){
+		if (err) return next(err);
+		if (!client) {
+			var error = new Error();
+			error.status = 400;
+			error.error_description = 'Client credentials are invalid';
+			error.error = 'invalid_grant';
+			return next(error);
 
-	if (token_type_hint === 'refresh_token') {
-		oAuthRefreshTokenModel.revokeRefreshToken(token, function(err, revokedToken){
-			if (err) return next(err);
-			console.log('-------------1');
-			res.sendStatus(200);
+		} 
 
-		});
-	}else if (token_type_hint === 'access_token') {
-		oAuthAccessTokenModel.revokeAccessToken(token, function(err, revokedToken){
-			if (err) return next(err);
-			res.sendStatus(200);
-						console.log('--------2');
+		var token_type_hint = req.body.token_type_hint;
+		var token = req.body.token;
 
-		});
-	}
+		if (token_type_hint === 'refresh_token') {
+			oAuthRefreshTokenModel.revokeRefreshToken(token, function(err, revokedToken){
+				if (err) return next(err);
+				if (!revokedToken) {
+					var error = new Error();
+					error.status = 404;
+					error.error_description = 'Token not found';
+					error.error = 'Not Found';
+					return next(error);
+				}
+				res.sendStatus(200);
+
+			});
+		}else if (token_type_hint === 'access_token') {
+			oAuthAccessTokenModel.revokeAccessToken(token, function(err, revokedToken){
+				if (err) return next(err);
+				if (!revokedToken) {
+					var error = new Error();
+					error.status = 404;
+					error.error_description = 'Token not found';
+					error.error = 'Not Found';
+					return next(error);
+				}
+				res.sendStatus(200);
+
+			});
+		}
+
+	});
+
+	
 
 
 });
