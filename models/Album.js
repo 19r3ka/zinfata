b47219d1dbdfe11c5a7549f4e78faf7e5172e777
modeste <mo.assiongbon@gmail.com>
2016-03-18 19:51:40 +0000

    update Album model : set the Album image default url

--- models/Album.js
+++ models/Album.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var AlbumSchema = new mongoose.Schema({
   title:        { type: String, required: true, lowercase: true },
-  imageUrl:     { type: String },
+  imageUrl:     { type: String, default: 'zinfataClient/assets/images/album-coverart-placeholder.png' },
   artistId:     { type: String, required: true },
   releaseDate:  { type: Date, required: true },
   updated_at:   { type: Date, default: Date.now }
