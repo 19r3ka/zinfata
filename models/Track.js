4c1570132cbf4049eeeaa038af34456e0a9c12f1
modeste <mo.assiongbon@gmail.com>
2016-03-27 22:45:16 +0000

    restore title and title_lower fields of Track model (accidently delete when doing a merge)

--- models/Track.js
+++ models/Track.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 
-var TrackSchema = new mongoose.Schema( {
-  artistId:  	  { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  feat:       	{ type: [{ type: mongoose.Schema.ObjectId, ref: 'User'}]},      // for the IDs of all contributing artists
+var TrackSchema = new mongoose.Schema({
+  title:        {type: String, required: true},
+  titleLower:  {type: String, lowercase: true, select: false},
+  artistId:  	  {type: mongoose.Schema.ObjectId, ref: 'User', required: true},
+  feat:       	{type: [{type: mongoose.Schema.ObjectId, ref: 'User'}]},      // for the IDs of all contributing artists
   size:         {type: String, required: true},
   duration:     {type: String, required: true},
   sharing:      {type: Boolean, default: false}, // should the track be available to others
