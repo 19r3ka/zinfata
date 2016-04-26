1578bc9406cf2a40b494ce596650d05e172c92ad
19r3ka <yemahiatsi@gmail.com>
2016-04-26 12:54:16 +0200

    Alert box now fades out after 2 secs.

--- public/zinfataClient/app/shared/messages/messageController.js
+++ public/zinfataClient/app/shared/messages/messageController.js
@@ -1,5 +1,6 @@
-app.controller('messageCtrl', ['$scope', 'MessageSvc', '$log',
-function($scope, MessageSvc, $log) {
+app.controller('messageCtrl', ['$scope', 'MessageSvc', '$log', '$timeout',
+function($scope, MessageSvc, $log, timeout) {
+  var e = angular.element('.alert-dismissable');
   $scope.$watch(function() {
     return MessageSvc.getMsg();
   }, function(newVal, oldVal) {
@@ -7,6 +8,9 @@ function($scope, MessageSvc, $log) {
       $scope.message = MessageSvc.getMsg();
       $scope.icon    = ($scope.message.type === 'success') ?
       'fa fa-check' : 'fa fa-exclamation';
+      timeout(function() {
+        e.alert('close');
+      }, 2000);
     }
   });
 }]);
