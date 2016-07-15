860342b78f1b13ac5d1bb6e0b20e55689ca64f2e
19r3ka <yemahiatsi@gmail.com>
2016-07-15 12:46:01 +0200

    Adds Google Analytics snippet, cdn, and autotrack.

--- config/assets/default.js
+++ config/assets/default.js
@@ -44,3 +44,4 @@ module.exports = {
     routes: ['routes/*.js']
   }
 };
+
---
+++ public/lib/google-analytics/zinfata-ga.js
@@ -0,0 +1,7 @@
+window.ga = window.ga || function () {
+  (ga.q = ga.q || []).push(arguments) };
+ga.l = +new Date;
+ga('create', 'UA-80702504-1', 'auto');
+ga('require', 'autotrack');
+ga('send', 'pageview');
+
